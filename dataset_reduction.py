9e857d87a9228fcd29f66c6b348b15405850f28b
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2025-05-06 12:05:49 +0200

    class reduction option

--- dataset_reduction.py
+++ dataset_reduction.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("datasets/hurricanes-point-feats-extracted.csv")
+df = pd.read_csv("datasets/ais-point-feats-extracted.csv")
 
-top_labels = df["label"].value_counts().nlargest(4).index
+top_labels = df["label"].value_counts().index
 
 filtered_df = df[df["label"].isin(top_labels)]
 
 print(filtered_df["label"].value_counts())
 
-filtered_df.to_csv(
-    "datasets/hurricanes_point_feats_extracted_top_4_classes_dataset.csv",
-    index=False,
-)
-
-
+# filtered_df.to_csv(
+#     "datasets/hurricanes_point_feats_extracted_top_4_classes_dataset.csv",
+#     index=False,
+# )
