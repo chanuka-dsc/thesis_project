efbfa4d2ee086450593d465ab84c3ed0fd4e4a0e
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2025-05-10 11:16:03 +0200

    reabse

--- baseline_hyper.py
+++ baseline_hyper.py
@@ -8,7 +8,7 @@ from xgboost import XGBClassifier
 from utilities import evaluate_with_cv_seeds_and_feature_logging_with_hyper
 
 # === Load and Prepare Dataset ===
-df = pd.read_csv("Datasets/ais_filtered_classes.csv")
+df = pd.read_csv("datasets/ais_filtered_classes.csv")
 
 X = df.drop(columns=["tid", "label"], errors="ignore")
 y = LabelEncoder().fit_transform(df["label"])
@@ -18,15 +18,15 @@ models = {
     # "Logistic Regression": LogisticRegression(
     #     solver="liblinear", random_state=42, max_iter=10000
     # ),
-    # # "Random Forest": RandomForestClassifier(n_jobs=-1, random_state=42),
-    # "XGBoost": XGBClassifier(random_state=42, eval_metric="mlogloss", n_jobs=-1),
-    "MLP": MLPClassifier(
-        hidden_layer_sizes=(10, 5),
-        activation="relu",
-        solver="adam",
-        max_iter=15000,
-        random_state=42,
-    ),
+    # "Random Forest": RandomForestClassifier(n_jobs=-1, random_state=42),
+    "XGBoost": XGBClassifier(random_state=42, eval_metric="mlogloss", n_jobs=-1),
+    # "MLP": MLPClassifier(
+    #     hidden_layer_sizes=(10, 5),
+    #     activation="relu",
+    #     solver="adam",
+    #     max_iter=15000,
+    #     random_state=42,
+    # ),
 }
 
 # === Define hyperparameter grids ===
@@ -77,8 +77,6 @@ for name, model in models.items():
     )
 
     df_all = pd.concat([result_forward, result_backward], ignore_index=True)
-    csv_path = (
-        f"results/csv/base/ais/{name.lower().replace(' ', '_')}_tuned_selection_f1_scores.csv"
-    )
+    csv_path = f"results/csv/base/ais/{name.lower().replace(' ', '_')}_tuned_selection_f1_scores.csv"
     df_all.to_csv(csv_path, index=False)
     print(f"Saved tuned selection CSV for {name}: {csv_path}")
