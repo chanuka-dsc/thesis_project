dc1510bb554d09764c3cb2a47fe32d3b139cdac3
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2025-05-16 09:54:12 +0200

    hurricane basline hyper xgboost and all taxonomy results

--- taxonomy_results.py
+++ taxonomy_results.py
@@ -11,7 +11,7 @@ import os
 from itertools import combinations
 
 # === Load and Prepare Dataset ===
-df = pd.read_csv("datasets/ais_filtered_classes.csv")
+df = pd.read_csv("datasets/hurricane_balanced_top5_classes.csv")
 X = df.drop(columns=["tid", "label"], errors="ignore")
 y = LabelEncoder().fit_transform(df["label"])
 
@@ -66,7 +66,7 @@ for name, model in models.items():
         # Save detailed results per combination
         model_safe = name.lower().replace(" ", "_")
         combo_safe = desc.replace(" ", "_")
-        csv_name = f"results/csv/taxonomy/ais/partial/{model_safe}_combo_{combo_safe}.csv"
+        csv_name = f"results/csv/taxonomy/hurricane/partial/{model_safe}_combo_{combo_safe}.csv"
         result_combo.to_csv(csv_name, index=False)
         print(f"Saved: {csv_name}")
 
@@ -80,7 +80,9 @@ for name, model in models.items():
     summary_df = pd.DataFrame(all_results).sort_values(
         by="mean_f1_weighted", ascending=False
     )
-    summary_path = f"results/csv/taxonomy/ais/{model_safe}_combination_summary.csv"
+    summary_path = (
+        f"results/csv/taxonomy/hurricane/{model_safe}_combination_summary.csv"
+    )
     summary_df.to_csv(summary_path, index=False)
     print(f"Saved summary for {name}: {summary_path}")
 
